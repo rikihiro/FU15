61fa6d75fdf756231bd73715553b17f903b780fc
Rikihiro Imai <rikihiro.imai37@gmail.com>
2024-07-04 16:31:35 +0900

    HoT: initial revision

---
+++ cointflip.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Tossing a coin...\n");
+    int h=0, t=0;
+    for(int i=1;i<4;i++)
+    {
+        int r = rand();
+        if(r%2 == 1) 
+        {
+            printf("Round %d: Heads\n", i); 
+            h++;
+        }
+        else 
+        {
+            printf("Round %d: Tails\n", i); 
+            t++;
+        }
+    }
+    printf("Heads: %d, Tails: %d\n", h, t);
+}
